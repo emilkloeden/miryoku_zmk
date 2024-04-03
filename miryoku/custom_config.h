457bd77928c8572e4aea5c2845edbc48b7302ccf
Emil Kloeden <emilkloeden@gmail.com>
2024-04-03 16:11:23 +1030

    Attempting to restore underglow

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+//# Uncomment the following lines to enable the Corne RGB Underglow
+//#define CONFIG_ZMK_RGB_UNDERGLOW=y
+//#define CONFIG_WS2812_STRIP=y
+
+// # Uncomment the following line to enable the Corne OLED Display
+//#define CONFIG_ZMK_DISPLAY=y
