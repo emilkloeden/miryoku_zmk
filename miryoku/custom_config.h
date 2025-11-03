54c0fbac112f9fece0dabb7952715af85433c771
Emil Kloeden <emilkloeden@gmail.com>
2025-11-04 10:18:18 +1030

    uncomment

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,8 +2,8 @@
 // https://github.com/manna-harbour/miryoku
 
 //# Uncomment the following lines to enable the Corne RGB Underglow
-//#define CONFIG_ZMK_RGB_UNDERGLOW=y
-//#define CONFIG_WS2812_STRIP=y
+#define CONFIG_ZMK_RGB_UNDERGLOW=y
+#define CONFIG_WS2812_STRIP=y
 
 // # Uncomment the following line to enable the Corne OLED Display
-//#define CONFIG_ZMK_DISPLAY=y
+#define CONFIG_ZMK_DISPLAY=y
